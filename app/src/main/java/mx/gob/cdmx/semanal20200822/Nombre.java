3511a019bca03fa9c22007d8828cd3ddeecc6f57
cpico5 <cpico56@gmail.com>
2020-08-19 14:16:36 -0500

    carga inicial

---
+++ app/src/main/java/mx/gob/cdmx/semanal20200822/Nombre.java
@@ -0,0 +1,25 @@
+package mx.gob.cdmx.semanal20200822;
+
+public class Nombre  {
+
+    public static final String customURL = "https://opinion.cdmx.gob.mx/encuestas/";
+    public static final String encuesta = "semanal_20200815";
+    public static final String USUARIO = "usuario";
+    public static final String PADRON = "padron";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String IMEI = "imei";
+
+    public String nombreEncuesta(){
+
+        final String nombreEncuesta = "semanal_20200815";
+        return nombreEncuesta;
+    }
+
+    public String nombreDatos(){
+
+        final String nombreEncuesta = "datos_semanal_20200815";
+        return nombreEncuesta;
+    }
+
+}	 
