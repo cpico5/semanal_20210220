70bf5c27e659bcff38eae7d693d153e6c6981775
cpico5 <cpico56@gmail.com>
2020-11-05 23:13:01 -0600

    inicial2

---
+++ app/src/main/java/mx/gob/cdmx/semanal_20201107/db/Anotaciones/AutoIncrement.java
@@ -0,0 +1,11 @@
+package mx.gob.cdmx.semanal_20201107.db.Anotaciones;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by KC on 5/11/2016.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoIncrement {
+}
