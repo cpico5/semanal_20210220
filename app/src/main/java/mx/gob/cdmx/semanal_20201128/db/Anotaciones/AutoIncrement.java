9081abf623a0169c87c14473af3e3ffb5e7b6c70
cpico5 <cpico56@gmail.com>
2020-11-27 15:24:02 -0600

    v3

---
+++ app/src/main/java/mx/gob/cdmx/semanal_20201128/db/Anotaciones/AutoIncrement.java
@@ -0,0 +1,11 @@
+package mx.gob.cdmx.semanal_20201128.db.Anotaciones;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by KC on 5/11/2016.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoIncrement {
+}
---
+++ app/src/main/java/mx/gob/cdmx/semanal_20201128/model/Usuarios.java
@@ -0,0 +1,47 @@
+package mx.gob.cdmx.semanal_20201128.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+import mx.gob.cdmx.semanal_20201128.db.Anotaciones.PrimaryKey;
+
+public class Usuarios implements Serializable {
+
+
+
+    @PrimaryKey
+
+    @SerializedName("usuario")
+    public String usuario;
+    @SerializedName("password")
+    public String password;
+
+
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
